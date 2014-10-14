cbb8ea6eb4f914dc78661176e4366d8d093f03b3
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2014-10-14 15:16:35 -0300

    Add updated ActsAsTokenAuthenticationHandler specs

--- spec/lib/simple_token_authentication/acts_as_token_authentication_handler_spec.rb
+++ spec/lib/simple_token_authentication/acts_as_token_authentication_handler_spec.rb
@@ -188,3 +188,31 @@ describe 'A class which can act as token authentication handler (or one of its c
     end
   end
 end
+
+describe SimpleTokenAuthentication::ActsAsTokenAuthenticationHandler do
+
+  after(:each) do
+    ensure_examples_independence
+  end
+
+  before(:each) do
+    define_test_subjects_for(SimpleTokenAuthentication::ActsAsTokenAuthenticationHandler)
+  end
+
+  describe '#acts_as_token_authentication_handler_for' do
+
+    it 'ensures the receiver class does handle token authentication for a given (token authenticatable) model' do
+      double_user_model
+
+      @subjects.each do |subject|
+        subject.stub(:before_filter)
+
+        expect(subject).to receive(:include).with(SimpleTokenAuthentication::TokenAuthenticationHandler)
+        expect(subject).to receive(:include).with(SimpleTokenAuthentication::ActsAsTokenAuthenticationHandlerMethods)
+        expect(subject).to receive(:handle_token_authentication_for).with(User, { option: 'value' })
+
+        subject.acts_as_token_authentication_handler_for User, { option: 'value' }
+      end
+    end
+  end
+end
